882bc875480475fe1893b4c1be30c3d48f1f68f1
tommy-mor <thmorriss@gmail.com>
2018-06-22 21:07:54 -0700

    made some basic ui elements

--- front/src/front/core.cljs
+++ front/src/front/core.cljs
@@ -2,11 +2,30 @@
     (:require
       [reagent.core :as r]))
 
+(defonce list-of-events
+  (r/atom [["brush teeth" "10 mins"] ["work on project" "2 hours"] ["sleep" "8 hours"]]))
 ;; -------------------------
 ;; Views
+(defn main-panel [what children]
+  [:div {:style {:background "pink" :padding "10px"}}
+    children])
+
+(defn line [a b]
+  [:div.line a [:span.right b]])
+
+(defn group [children]
+  (let [selected nil]
+    (fn [children]
+      [:div
+       (for [[a b] children]
+         (line a b))])))
 
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [main-panel
+   ;;(js/console.log list-of-events)
+   (println @list-of-events)
+   [:div [:h4 "Welcome to Generic UI"]
+    [group @list-of-events]]])
 
 ;; -------------------------
 ;; Initialize app
